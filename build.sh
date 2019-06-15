76a40b0468873e2be7e96aebad84754252b9e397
rjplaton <rjplaton@gmail.com>
2019-06-14 23:59:24 -0700

    Added another index.html in the root so the site would resolve at the root

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+echo "Building docs from content and templates"
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
 cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
 cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+echo "Currently building another index so that the root resolves properly"
+cat templates/top.html content/index.html templates/bottom.html > ./index.html
