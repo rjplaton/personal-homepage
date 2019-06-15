48358de5553876d18221e4765afab16cf5b889be
rjplaton <rjplaton@gmail.com>
2019-06-14 23:00:54 -0700

    build script now includes blog, contact and projects

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
