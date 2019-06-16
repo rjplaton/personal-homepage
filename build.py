6aa42293bc79ce02e9d4b3f120a9d3f81d757f61
rjplaton <rjplaton@gmail.com>
2019-06-15 22:07:11 -0700

    build.py now builds all current pages from content and templates

--- build.py
+++ build.py
@@ -19,3 +19,13 @@ content = open('./content/blog.html').read()
 blog_page = top + content + bottom
 open('./docs/blog.html', 'w+').write(blog_page)
 print('Successfully built the blog.html page')
+
+content = open('./content/projects.html').read()
+projects_page = top + content + bottom
+open('./docs/projects.html', 'w+').write(projects_page)
+print('Successfully built the projects.html page')
+
+content = open('./content/contact.html').read()
+contact_page = top + content + bottom
+open('./docs/contact.html', 'w+').write(contact_page)
+print('Successfully built the contact.html page')
