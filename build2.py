49bb143b9baa57e09d2b1457940be7a8ab06ad94
rjplaton <rjplaton@gmail.com>
2019-06-15 23:12:34 -0700

    build2.py can now build all pages with a single template file

--- build2.py
+++ build2.py
@@ -14,3 +14,31 @@ index_page = template.safe_substitute(
     content=index_content,
 )
 open('./docs/index.html', 'w+').write(index_page)
+
+#repeating above for blog, projects, contact
+
+blog_content = open('./content/blog.html').read()
+blog_page = template.safe_substitute(
+    title="Blog",
+    blog_class="current-item",
+    content=blog_content,
+)
+open('./docs/blog.html', 'w+').write(blog_page)
+
+
+projects_content = open('./content/projects.html').read()
+projects_page = template.safe_substitute(
+    title="projects",
+    projects_class="current-item",
+    content=projects_content,
+)
+open('./docs/projects.html', 'w+').write(projects_page)
+
+
+contact_content = open('./content/contact.html').read()
+contact_page = template.safe_substitute(
+    title="contact",
+    contact_class="current-item",
+    content=contact_content,
+)
+open('./docs/contact.html', 'w+').write(contact_page)
