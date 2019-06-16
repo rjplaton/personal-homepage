286eb66ec723f96090863db64c1c3503d982e7b2
rjplaton <rjplaton@gmail.com>
2019-06-15 23:22:08 -0700

    tweaks to title and added meta description to template

--- build2.py
+++ build2.py
@@ -9,9 +9,10 @@ template = Template(template_text)
 #build the index.html page using template.html
 index_content = open('./content/index.html').read()
 index_page = template.safe_substitute(
-    title="Reuben J. Platon",
+    title="Personal Projects & More",
     index_class="current-item",
     content=index_content,
+    meta_description="Welcome to Reuben's personal project site"
 )
 open('./docs/index.html', 'w+').write(index_page)
 
@@ -22,23 +23,26 @@ blog_page = template.safe_substitute(
     title="Blog",
     blog_class="current-item",
     content=blog_content,
+    meta_description="Blogging about stuff"
 )
 open('./docs/blog.html', 'w+').write(blog_page)
 
 
 projects_content = open('./content/projects.html').read()
 projects_page = template.safe_substitute(
-    title="projects",
+    title="Projects",
     projects_class="current-item",
     content=projects_content,
+    meta_description="Current and past coding projects and more"
 )
 open('./docs/projects.html', 'w+').write(projects_page)
 
 
 contact_content = open('./content/contact.html').read()
 contact_page = template.safe_substitute(
-    title="contact",
+    title="Contact Me",
     contact_class="current-item",
     content=contact_content,
+    meta_description="Contact me through the contact form on this page"
 )
 open('./docs/contact.html', 'w+').write(contact_page)
