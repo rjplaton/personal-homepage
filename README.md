815f5349ee46a5a4d7fa11e9dfe00566d9abecc3
rjplaton <rjplaton@gmail.com>
2019-06-23 00:07:31 -0700

    build3.py is a refactor of build2.py using a for loop iterating through a dictionary

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 #personal-homepage
-Project to build a personal homepage
+Project to build a personal homepage using
 
 Updated Instructions:
 To make changes to pages, make them in either /content/*.html  or /template/template/html
-Run build2.py to combine /content + /template into full pages that live in /docs.
-
-
-~~Instructions:
-To make changes to pages, make them in either /content  or /template
-Run build.sh or build.py to combine the fragments from /content + /templates into full pages that live in /docs~~
-
-Test
+Run build3.py to combine /content + /template into full pages that live in /docs.
---
+++ build3.py
@@ -0,0 +1,43 @@
+from string import Template
+
+template_text = open('./templates/template.html').read()
+template = Template(template_text)
+
+pages = [ {
+        "filename": "content/index.html",
+        "output": "docs/index.html",
+        "title": "Personal Projects & More",
+        "meta_description": "Welcome to Reuben's personal project site",
+        "index_class": 'class="current-item"',
+}, 
+{
+        "filename": "content/blog.html",
+        "output": "docs/blog.html",
+        "title": "Blog",
+        "meta_description": "Blogging about stuff",
+        "blog_class": 'class="current-item"',
+}, 
+{
+        "filename": "content/contact.html",
+        "output": "docs/contact.html",
+        "title": "Contact Me",
+        "meta_description": "Contact me through the contact form on this page",
+        "contact_class": 'class="current-item"',
+},
+{
+        "filename": "content/projects.html",
+        "output": "docs/projects.html",
+        "title": "Projects & More",
+        "meta_description": "Current & Past Coding Projects",
+        "projects_class": 'class="current-item"',
+},
+]
+
+for page in pages:
+    print(page['filename'])
+    full_page = template.safe_substitute(page)
+    open(page['output'], 'w+').write(full_page)
+    content = open(page['filename']).read()
+    full_page = full_page.replace("{content}", content)
+    open(page['output'], 'w+').write(full_page)
+    print('Completed: ',page['output'])
