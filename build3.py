33ebc53c8f49a43f5b5a9c01d080265465b54053
rjplaton <rjplaton@gmail.com>
2019-06-23 10:17:18 -0700

    putting the for loop in a function

--- build3.py
+++ build3.py
@@ -3,6 +3,7 @@ from string import Template
 template_text = open('./templates/template.html').read()
 template = Template(template_text)
 
+#list of every page and the strings to replace
 pages = [ {
         "filename": "content/index.html",
         "output": "docs/index.html",
@@ -33,11 +34,17 @@ pages = [ {
 },
 ]
 
-for page in pages:
-    print(page['filename'])
-    full_page = template.safe_substitute(page)
-    open(page['output'], 'w+').write(full_page)
-    content = open(page['filename']).read()
-    full_page = full_page.replace("{content}", content)
-    open(page['output'], 'w+').write(full_page)
-    print('Completed: ',page['output'])
+#function to replace template values
+def replace_template_values():
+    for page in pages:
+        print(page['filename'])
+        full_page = template.safe_substitute(page)
+        open(page['output'], 'w+').write(full_page)
+
+        #replace the content section with /content/ pages
+        content = open(page['filename']).read()
+        full_page = full_page.replace("{content}", content)
+        open(page['output'], 'w+').write(full_page)
+        print('Completed: ',page['output'])
+
+replace_template_values()
